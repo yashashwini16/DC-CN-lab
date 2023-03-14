6c42540b67a700d16f185684d23a39308d51bdb0
yashashwini16 <yashaswinib1603@gmail.com>
2023-03-14 05:41:19 +0530

    changesmade

--- EServer.java
+++ EServer.java
@@ -6,9 +6,12 @@ public static void main(String args[])
 {
 ServerSocket s=null;
 String line;
-DataInputStream is;
+
 PrintStream ps;
 Socket c=null;
+
+BufferedReader is
+          = new BufferedReader(new InputStreamReader(System.in));
 try
 {
 s=new ServerSocket(9000);
@@ -20,7 +23,7 @@ System.out.println(e);
 try
 {
 c=s.accept();
-is=new DataInputStream(c.getInputStream());
+
 ps=new PrintStream(c.getOutputStream());
 while(true)
 {
